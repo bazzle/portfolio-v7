8f6d094f7da96552fac4bd443c012b6b4b33601c
Barry Richards <barry@br-web.me>
2024-09-12 21:11:07 +0100

    Header styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,9 +4,13 @@ function Header(){
             <div className="container header__inner">
                 <h1 className="header__site-name">
                     <span className="name">Barry Richards</span>
-                    <span>Web Designer</span>
+                    <span>Web Engineer</span>
                 </h1>
-                <hr className="header__line" />
+				<div className="header__line">
+					<hr />
+					<svg width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" /></svg>
+				</div>
             </div>
         </header>
     )
