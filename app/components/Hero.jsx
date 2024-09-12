6ad83962db7574375b920afff223d4f8df0737d1
Barry Richards <barry@br-web.me>
2024-09-12 22:11:40 +0100

    Hero progress

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,21 +6,26 @@ function Hero(){
         <section className="hero">
             <div className="container">
                 <div className="hero__inner">
-                    <div className="hero__message">
-                        <p className="hero__message__main">
-                            My philosophy is <br/><span className="highlight">Web for everyone.</span>
-                        </p>
-                        <p className="hero__message__sub">
+                    <p className="hero__message">
+						<span className="hero__message__sub">
+                            My philosophy is
+                        </span>
+                        <span className="hero__message__main">
+							Web for <span className="highlight--alt">everyone.</span>
+                        </span>
+                        <span className="hero__message__sub">
                             ... But still <span className="highlight">look cool</span>.
-                        </p>
-                    </div>
-                    <div className="hero__links">
-                        <a href="mailto:barry@br-web.me">barry@br-web.me</a> &mdash;
-                        <a href="https://www.linkedin.com/in/br-web/">LinkedIn</a>
-                    </div>
+                        </span>
+                    </p>
                     <VerticalDots/>
                 </div>
             </div>
+			<div className="container">
+				<div className="hero__links">
+                    <a href="mailto:barry@br-web.me">barry@br-web.me</a> &mdash;
+                    <a href="https://www.linkedin.com/in/br-web/">LinkedIn</a>
+				</div>
+			</div>
         </section>
     )
 }
