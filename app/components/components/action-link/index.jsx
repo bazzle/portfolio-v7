83b3cd8ab4c0fbef9537385086429429cb441540
Barry Richards <barry@br-web.me>
2024-09-19 16:13:32 +0100

    Refactor CSS for modules
    Redo classnames with modular syntax
    Add modular folder structure with dedicated stylesheets

--- app/components/components/action-link/index.jsx
+++ app/components/components/action-link/index.jsx
@@ -1,8 +1,9 @@
+import styles from './style.module.scss';
 import propTypes from "prop-types";
 
 function ActionLink({linkString, textString}){
 	return (
-		<div className="c-action-link">
+		<div className={styles.action_link}>
 			<svg className="dot" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" /></svg>
 			<a href={linkString}>{textString}</a>
 		</div>
