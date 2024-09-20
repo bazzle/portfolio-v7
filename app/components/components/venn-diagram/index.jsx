dd3c19b7251f66119614ae6125ee424a9cfbb6a7
Barry Richards <barry@br-web.me>
2024-09-21 00:52:14 +0100

    Add venn diagram

--- app/components/components/venn-diagram/index.jsx
+++ app/components/components/venn-diagram/index.jsx
@@ -1,11 +1,39 @@
 import styles from './style.module.scss';
+import LoopActions from './loop-actions.jsx';
 
-function VennDiagram(){
+function VennDiagram() {
+
     return (
         <div className={styles.venn_diagram}>
-			<p>Venn Diagram coming soon</p>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="300"
+                height="255"
+                viewBox="0 0 300 255"
+                fill="none"
+            >
+                <g clipPath="url(#clip0_0_1)">
+                    <circle cx="186.282" cy="140.384" r="112.718" stroke="#9AF3FF" strokeWidth="2" />
+                    <text fill="#9AF3FF" xmlSpace="preserve">
+                        <tspan x="155.957" y="10.6979">Development</tspan>
+                    </text>
+                    <circle cx="113.718" cy="140.384" r="112.718" stroke="#FFAAAA" strokeWidth="2" />
+                    <text fill="#FFAAAA" xmlSpace="preserve">
+                        <tspan x="86.6426" y="10.6979">Design</tspan>
+                    </text>
+					<text xmlSpace="preserve" className={styles.venn_diagram__text}>
+                        <tspan x="150" y="132.539" text-anchor="middle" dominant-baseline="middle">Space where I can</tspan>
+						<tspan x="150" y="150.409"text-anchor="middle" dominant-baseline="middle">{<LoopActions />}</tspan>
+                    </text>
+                </g>
+                <defs>
+                    <clipPath id="clip0_0_1">
+                        <rect width="1200" height="3003.34" fill="white" transform="translate(-100 -1564)" />
+                    </clipPath>
+                </defs>
+            </svg>
         </div>
-    )
+    );
 }
 
 export default VennDiagram;
