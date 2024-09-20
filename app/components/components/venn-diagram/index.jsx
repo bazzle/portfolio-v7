21ba3d440065ef941a049c0f58ab35809156baa2
Barry Richards <barry@br-web.me>
2024-09-20 19:14:29 +0100

    Add diagram component

---
+++ app/components/components/venn-diagram/index.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+
+function VennDiagram(){
+    return (
+        <div className={styles.venn_diagram}>
+			<p>Venn Diagram coming soon</p>
+        </div>
+    )
+}
+
+export default VennDiagram;
