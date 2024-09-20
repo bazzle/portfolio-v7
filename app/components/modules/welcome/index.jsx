df54e1d82a1d0a59fcc4efcdd1bc84e2576f2f74
Barry Richards <barry@br-web.me>
2024-09-20 17:12:56 +0100

    Welcome section

---
+++ app/components/modules/welcome/index.jsx
@@ -0,0 +1,37 @@
+import styles from './style.module.scss';
+import SectionHead from "../../components/section-head";
+
+function Welcome(){
+	return(
+		<section className={styles.welcome}>
+			<div className="container pt--deep pb--deep">
+				<div className={styles.inner}>
+					<div className={styles.col_1}>
+						<SectionHead titleString="Welcome" separator />
+					</div>
+					<div className={styles.col_2}>
+						<div className={styles.body_text}>
+							<p>
+								From the baseline of accessibility I seek to bring understated creative flair, using typography, hierarchy, animation and colour. I’m experienced at working with clients to help unlock that missing piece, that elusive “pop” that their website needs.
+							</p>
+							<p>
+								I’m a design minimalist, I believe small changes make big differences. I take a pragmatic approach and I’m sympathetic to client and user needs. 
+							</p>
+							<p className="inset-text">
+								I call myself a design engineer because I like to occupy the intersection of visual design and web development. I find the two disciplines equally captivating.
+							</p>
+							<p>
+								I have experience in many phases of development. Initial prototyping, user testing, design and build of the component library, scoping sessions with the team and ongoing iteration and maintenance.
+							</p>
+							<p>
+								Currently : learning React and NextJS (about time) and some personal projects
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Welcome;
--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,20 @@
 // import Image from "next/image";
-
 import Hero from "./components/modules/hero";
 import SkillsAndAbilities from "./components/modules/skills-and-abilities";
 import Typesheet from "./components/modules/type-sheet/type-sheet";
 import WorkHistory from "./components/modules/work-history";
+import Welcome from "./components/modules/welcome";
 
 
 function Home(){
 	return(
 		<main>
 			<Hero/>
+			<Welcome/>
 			<SkillsAndAbilities/>
 			<WorkHistory/>
 			<Typesheet/>
+			
 		</main>
 	)
 }
