5f7ece1ce5d6d10cba698acadf413a3a72960efc
Barry Richards <barry@br-web.me>
2024-09-20 11:57:19 +0100

    Fix linking issues and remove unused stylesheets

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@
 import Hero from "./components/modules/hero";
 import SkillsAndAbilities from "./components/modules/skills-and-abilities";
 import Status from "./components/components/status";
-import Typesheet from "./components/modules/type-sheet";
+import Typesheet from "./components/modules/type-sheet/type-sheet";
 import WorkHistory from "./components/modules/work-history";
 
 
