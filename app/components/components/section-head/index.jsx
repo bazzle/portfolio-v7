28f1e049b3c786889e2d367104a67447808919b3
Barry Richards <barry@br-web.me>
2024-09-20 13:22:23 +0100

    Refactor and styling refinement on section head

--- app/components/components/section-head/index.jsx
+++ app/components/components/section-head/index.jsx
@@ -1,13 +1,22 @@
 import propTypes from "prop-types";
-
+import styles from "./styles.module.scss";
 function SectionHead({
     titleString = 'Section title',
     separator = false
 }){
-    const classNameString = `section-head ${separator && 'section-head--separator'}`;
+	const separatorElem = () => {
+		return (
+			<div className={styles.separator}>
+				<svg viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" /></svg>
+			</div>
+		)
+	}
     return (
-        <div className={classNameString}>
-            <h2 className="section-head__title text-section-title">{titleString}</h2>
+        <div className={styles.section_head}>
+			{separator && separatorElem()}
+            <h2 className={separator ? styles.title : styles.title__no_separator}>
+				{titleString}
+			</h2>
         </div>
     )
 }
