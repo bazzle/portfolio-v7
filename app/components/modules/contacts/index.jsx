6e4e83c474f4baed2069aa3725b884d61a219858
Barry Richards <barry@br-web.me>
2024-09-21 21:41:58 +0100

    Updates and add client logos

---
+++ app/components/modules/contacts/index.jsx
@@ -0,0 +1,18 @@
+import styles from './style.module.scss';
+import ActionLink from '../../components/action-link';
+
+function Contacts() {
+	return (
+		<div className={styles.contacts}>
+			<div className="container container--vertical-fill">
+				<div className={styles.inner}>
+					<ActionLink linkString="mailto:barry@br-web.me" textString="Drop me an email" />
+					<ActionLink linkString="https://www.linkedin.com/in/br-web/" textString="LinkedIn" />
+					<ActionLink linkString="https://github.com/bazzle/" textString="Github" />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Contacts;
