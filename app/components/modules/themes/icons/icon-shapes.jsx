d677b7cc2f59d5ba3980ea701149606dd392083f
Barry Richards <barry@br-web.me>
2024-09-29 18:59:23 +0100

    Update  skills section

--- app/components/modules/themes/icons/icon-shapes.jsx
+++ app/components/modules/themes/icons/icon-shapes.jsx
@@ -1,9 +1,9 @@
 function IconShapes(){
 	return(
 		<svg width="113" height="87" viewBox="0 0 113 87" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<rect x="1.12134" y="0.592773" width="55.3808" height="55.3808" fill="#202020" stroke="#FFAAAA" stroke-width="0.5"/>
+			<rect x="1.12134" y="0.592773" width="55.3808" height="55.3808" fill="#202020" stroke="#FFAAAA" strokeWidth="0.5"/>
 			<rect x="30.7822" y="31.5142" width="54.8808" height="54.8808" fill="#202020" stroke="white"/>
-			<rect x="57.3318" y="1.34277" width="53.8808" height="53.8808" fill="#202020" stroke="#9AF3FF" stroke-width="2"/>
+			<rect x="57.3318" y="1.34277" width="53.8808" height="53.8808" fill="#202020" stroke="#9AF3FF" strokeWidth="2"/>
 		</svg>
 
 	)
