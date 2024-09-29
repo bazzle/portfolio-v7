4010bb540ebf5421212783125f6c091975425ff1
Barry Richards <barry@br-web.me>
2024-09-30 00:47:01 +0100

    Work history and some styling updates

--- app/components/modules/work-history/content-sheet/index.jsx
+++ app/components/modules/work-history/content-sheet/index.jsx
@@ -3,45 +3,45 @@ import PropTypes from 'prop-types';
 
 function ContentSheet({
 		name,
-		duration,
+		startDate,
+		endDate,
 		clients,
 		intro,
 		bodyContent
 	}) {
-	return (
-		<div className={styles.content_sheet}>
-			<div className={styles.inner}>
-				<div className={styles.metadata}>
-					<div className={styles.metadata__item}>
-						<h2 className={styles.title}>{name}</h2>
+		const duration = endDate ? `${startDate - endDate} years` : 'Ongoing';
+		return (
+			<div className={styles.content_sheet}>
+				<div className="inner-2col">
+					<div className={styles.content_sheet__main}>
+						<p className={styles.intro}>
+							{intro}
+						</p>
+						<div className={styles.body_text} dangerouslySetInnerHTML={{ __html: bodyContent }} />
+						<div className={styles.metadata}>
+							<div className={styles.metadata__item}>
+								<h3 className={styles.metadata__heading}>Duration</h3>
+								<p className={styles.duration}>{duration}</p>
+							</div>
+							<div className={styles.metadata__item}>
+								<h3 className={styles.metadata__heading}>Clients</h3>
+								<ul className={styles.clients_list}>
+									{clients.map((item, index) => {
+										return <li key={index}>{item}</li>
+									})}
+								</ul>
+							</div>
+						</div>
 					</div>
-					<div className={styles.metadata__item}>
-						<h3 className={styles.metadata__heading}>Duration</h3>
-						<p className={styles.duration}>{duration}</p>
-					</div>
-					<div className={styles.metadata__item}>
-						<h3 className={styles.metadata__heading}>Clients</h3>
-						<ul className={styles.clients_list}>
-						{clients.map((item, index) => {
-							return <li key={index}>{item}</li>
-						})}
-						</ul>
-					</div>
-				</div>
-				<div className={styles.about}>
-					<p className={styles.intro}>
-						{intro}
-					</p>
-					<div className={styles.body_text} dangerouslySetInnerHTML={{ __html: bodyContent }} />
 				</div>
 			</div>
-		</div>
-	)
+		)
 }
 
 ContentSheet.propTypes = {
 	name: PropTypes.string.isRequired,
-	duration: PropTypes.string.isRequired,
+	startDate: PropTypes.number.isRequired,
+	endDate: PropTypes.number,
 	clients: PropTypes.array.isRequired,
 	intro: PropTypes.string,
 	bodyContent: PropTypes.string.isRequired,
