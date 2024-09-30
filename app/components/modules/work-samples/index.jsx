54abf6c3770086a34584f2e32d559322888ae473
Barry Richards <barry@br-web.me>
2024-10-01 00:15:06 +0100

    Extra row of work samples

--- app/components/modules/work-samples/index.jsx
+++ app/components/modules/work-samples/index.jsx
@@ -4,30 +4,66 @@ import BrowserDesktop from './browser-desktop';
 function WorkSamples(){
     return(
         <section className={styles.work_samples}>
-			<div className={styles.title_overlay}>
-				<h2 className={styles.title_overlay_text}>Work Samples coming soon</h2>
-			</div>
 			<div className={styles.inner}>
-				<BrowserDesktop
-					src="/assets/images/samples-nao.png"
-					alt="profile"
-				/>
-				<BrowserDesktop
-					src="/assets/images/samples-bfi.png"
-					alt="profile"
-				/>
-				<BrowserDesktop
-					src="/assets/images/samples-nao.png"
-					alt="profile"
-				/>
-				<BrowserMobile
-					src="/assets/images/samples-satoshi-power.png"
-					alt="profile"
-				/>
-				<BrowserDesktop
-					src="/assets/images/samples-bfi.png"
-					alt="profile"
-				/>
+				<div className={styles.title_overlay}>
+					<h2 className={styles.title_overlay_text}>Work Samples coming soon</h2>
+				</div>
+				<div className={styles.strip}>
+					<BrowserMobile
+						src="/assets/images/samples-satoshi-power.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-ons.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-bfi.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-nao.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-ons.png"
+						alt="profile"
+					/>
+					<BrowserMobile
+						src="/assets/images/samples-satoshi-power.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-bfi.png"
+						alt="profile"
+					/>
+				</div>
+				<div className={styles.strip}>
+					<BrowserDesktop
+						src="/assets/images/samples-nao.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-bfi.png"
+						alt="profile"
+					/>
+					<BrowserMobile
+						src="/assets/images/samples-satoshi-power.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-nao.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-bfi.png"
+						alt="profile"
+					/>
+					<BrowserDesktop
+						src="/assets/images/samples-ons.png"
+						alt="profile"
+					/>
+				</div>
 			</div>
         </section>
     )
