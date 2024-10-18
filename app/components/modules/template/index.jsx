c7940f0e9c4eb0a6011580545e979c3521be5005
Barry Richards <barry@br-web.me>
2024-10-18 12:30:37 +0100

    Create text blockquote block

--- app/components/modules/template/index.jsx
+++ app/components/modules/template/index.jsx
@@ -2,13 +2,13 @@ import styles from "./style.module.scss";
 
 function Template(){
 	return(
-		<div className={styles.template}>
+		<section className={styles.template}>
 			<div className="container">
 				<div className={styles.inner}>
 					<p>Template module</p>
 				</div>
 			</div>
-		</div>
+		</section>
 	)
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -6,12 +6,14 @@ import Welcome from "./components/modules/welcome";
 import WorkSamples from "./components/modules/work-samples";
 import Themes from "./components/modules/themes";
 import Typesheet from "./components/modules/type-sheet/type-sheet";
+import TextBlock from "./components/modules/text-block";
 
 function Home(){
 	return(
 		<main>
 			<Hero/>
 			<Welcome/>
+			<TextBlock text="I call myself a design engineer because I like to occupy the intersection of visual design and web development. I find the two disciplines equally captivating."/>
 			<SkillsAndAbilities/>
 			<Themes/>
 			<WorkSamples/>
