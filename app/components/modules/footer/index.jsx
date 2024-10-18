0344e469e45d135c280585e68e2d4ef2396ab591
Barry Richards <barry@br-web.me>
2024-10-18 14:44:46 +0100

    Add back to top button

--- app/components/modules/footer/index.jsx
+++ app/components/modules/footer/index.jsx
@@ -1,4 +1,5 @@
 import styles from './style.module.scss';
+import BackTop from '../../components/backtop';
 
 function Footer(){
 
@@ -6,13 +7,14 @@ function Footer(){
 
     return(
         <footer className={styles.footer}>
-			<div className={styles.footer__inner}>
-				<div className="container">
-					<div className={styles.footer__line}>
-						<p>
-							&copy; Barry Richards – {currentYear}
-						</p>
-					</div>
+			<div className="container">
+				<div className={styles.footer__line}>
+					<p>
+						&copy; Barry Richards – {currentYear}
+					</p>
+				</div>
+				<div className={styles.back_top}>
+					<BackTop/>
 				</div>
 			</div>
         </footer>
