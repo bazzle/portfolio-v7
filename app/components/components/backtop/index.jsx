f200399ffef0792020f225b287a8ab33c1604a5e
Barry Richards <barry@br-web.me>
2024-10-18 14:44:46 +0100

    Add back to top button

---
+++ app/components/components/backtop/index.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+import styles from './style.module.scss';
+
+const handleClick = () => {
+	window.scrollTo({
+		top: 0,
+		behaviour: 'auto'
+	});
+}
+
+function BackTop(){
+	return(
+		<button aria-label='Back to top' className={styles.backTop} onClick={handleClick}>
+			<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M3 0.5H30C31.3807 0.5 32.5 1.61929 32.5 3V32.5H0.5V3C0.5 1.61929 1.61929 0.5 3 0.5Z" fill="#9AF3FF"/>
+				<path d="M3 0.5H30C31.3807 0.5 32.5 1.61929 32.5 3V32.5H0.5V3C0.5 1.61929 1.61929 0.5 3 0.5Z" stroke="#9AF3FF"/>
+				<path d="M23.5 19.5002L16.5 13.5002L9.50024 19.4998" stroke="black"/>
+			</svg>
+		</button>
+	)
+}
+
+export default BackTop;
