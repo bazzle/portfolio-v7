c40c5d9b93fc2c293bffcfd2e3cc99edccf40001
Barry Richards <barry@br-web.me>
2024-10-23 01:34:32 +0100

    Refactor colour mode switcher

--- app/components/modules/header/colour-mode.jsx
+++ app/components/modules/header/colour-mode.jsx
@@ -3,36 +3,38 @@
 import { useState, useEffect } from 'react';
 import styles from './style.module.scss';
 
-function DarkMode() {
-	const [isDarkMode, setIsDarkMode] = useState(false);
+function ColourMode() {
+	const [colorModeState, setColorModeState] = useState('dark');
+	let currentMode = localStorage.getItem('colorMode');
 
-	// Function to apply dark mode to the body and localStorage
-	const applyDarkMode = (isDark) => {
-		setIsDarkMode(isDark);
-		document.body.classList.toggle('light-mode', isDark);
-		localStorage.setItem('darkMode', isDark ? 'light' : 'dark');
-	};
+	const applyLightMode = () => {
+		document.body.classList.add('light-mode');
+		localStorage.setItem('colorMode', 'light');
+		setColorModeState('light');
+	}
+	const applyDarkMode = () => {
+		document.body.classList.remove('light-mode');
+		localStorage.setItem('colorMode', 'dark');
+		setColorModeState('dark');
+	}
 
 	useEffect(() => {
-		// Initialize dark mode state
-		const storedMode = localStorage.getItem('darkMode');
-		const initialDarkMode = storedMode === 'dark' || (storedMode === null && prefersDarkScheme.matches);
-		applyDarkMode(initialDarkMode);
-
-		// Event listener for system preference changes
-		const handleSystemPreferenceChange = (e) => {
-			applyDarkMode(e.matches);
-		};
-	}, []);
-
-	// Handle button click for toggling dark mode
+		if(currentMode == 'light'){
+			applyLightMode();
+		}
+	}, [currentMode]);
+	
 	const handleClick = () => {
-		const newDarkMode = !isDarkMode;
-		applyDarkMode(newDarkMode);
+		currentMode = localStorage.getItem('colorMode');
+		if(currentMode === 'light' || currentMode === null){
+			applyDarkMode();
+		} else {
+			applyLightMode();
+		}
 	};
 
 	return (
-		<button onClick={handleClick} aria-label={`Switch to ${isDarkMode ? 'light' : 'dark'} mode`} className={styles.dm_switcher}>
+		<button onClick={handleClick} aria-label={`Switch to ${colorModeState === 'light' ? 'dark' : 'light'} mode`} className={styles.dm_switcher}>
 			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path fillRule="evenodd" className="fill-foreground" clipRule="evenodd" d="M21.96 18.6954C19.8047 21.8952 16.1482 24 12 24C5.37258 24 0 18.6274 0 12C0 10.1897 0.400868 8.473 1.11868 6.93387C1.25436 6.60815 1.41836 6.2892 1.61145 5.98019L1.61543 5.98267C3.69274 2.40542 7.56546 0 12 0C18.6274 0 24 5.37258 24 12C24 14.4292 23.2782 16.6898 22.0374 18.5791C22.0136 18.6189 21.9893 18.6587 21.9646 18.6983L21.96 18.6954ZM21.5671 11.1992C21.1602 6.27196 17.0323 2.4 12 2.4C8.16405 2.4 4.85357 4.64984 3.31615 7.90193C2.57926 9.73643 3.25116 11.9029 5.00737 13.0003C6.97645 14.2307 9.55591 13.6469 10.7704 11.7033C12.6897 8.63177 16.7512 7.71243 19.8405 9.64281C20.5228 10.0692 21.101 10.5991 21.5671 11.1992Z"/>
 			</svg>
@@ -40,4 +42,4 @@ function DarkMode() {
 	);
 }
 
-export default DarkMode;
+export default ColourMode;
