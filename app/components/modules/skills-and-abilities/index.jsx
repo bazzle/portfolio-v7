2cceabf65f51f524f0ec9df4b3793106c5f7b111
Barry Richards <barry@br-web.me>
2024-10-25 16:36:53 +0100

    Mobile styling updates

--- app/components/modules/skills-and-abilities/index.jsx
+++ app/components/modules/skills-and-abilities/index.jsx
@@ -1,35 +1,35 @@
 import styles from './style.module.scss';
 import SectionHead from "../../components/section-head";
-import VennDiagram from '../../components/venn-diagram';
+import VennDiagram from './venn-diagram';
 import SkillsList from './skills-list';
 
 function SkillsAndAbilities(){
 
     return(
         <section className={styles.skills_abilities}>
-			<div className="container mb">
+			<div className="container">
 				<div className="inner-2col">
 					<div className="col-1">
 						<SectionHead titleString="Skills and expertise" separator/>
 					</div>
-					<div className="col-2">
+					<div className="col-2 mb">
 						<SkillsList />
 					</div>
 				</div>
 			</div>
 			<div className="container">
 				<div className="inner-2col">
-					<div className="col-1">
+					<div className="col-1 mb">
 						<div className={styles.diagram}>
 							<VennDiagram />
 						</div>
 					</div>
-					<div className="col-2">
+					<div className="col-2 {styles.main_section}">
 						<div className={styles.body_text}>
 							<p>
 								My happy place is being creative, using my code editor like a metaphorical canvas. Utilising the wealth of freely available languages and libraries. I have extensive experience with fundamental front-end technologies including vanilla javascript, and I’m getting to grips with React. WordPress has been my most commonly used content management system. 
 							</p>
-							<p className="inset-text">
+							<p className="outset-text">
 								I call myself a design engineer because I like to occupy the intersection of visual design and web development. I find the two disciplines equally captivating.
 							</p>
 							<p>
