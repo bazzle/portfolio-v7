7419a308ebc287fc5e20d9dd694d515540d563c8
Barry Richards <barry@br-web.me>
2024-10-26 16:29:02 +0100

    Make icons look more interesting in LM

--- app/components/modules/themes/icons/icon-mag-glass.jsx
+++ app/components/modules/themes/icons/icon-mag-glass.jsx
@@ -3,8 +3,8 @@ import styles from "./style.module.scss";
 function IconMagGlass({slug}){
 	return(
 		<svg className={styles[slug]} width="60" height="77" viewBox="0 0 56 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<circle cx="24.1119" cy="24.1051" r="22.494" className="stroke-accent-alt" strokeWidth="2.13766"/>
-			<rect x="37.5532" y="47.4784" width="3.96259" height="28.5379" transform="rotate(-30 37.5532 47.4784)" className="stroke-accent" strokeWidth="0.735623"/>
+			<circle cx="24.1119" cy="24.1051" r="22.494" className="stroke-accent-alt fill-accent-alt-faded" strokeWidth="3"/>
+			<rect x="37.5532" y="47.4784" width="3.96259" height="28.5379" transform="rotate(-30 37.5532 47.4784)" className="stroke-accent" strokeWidth="1"/>
 		</svg>
 	)
 }
--- app/components/modules/themes/icons/icon-shapes.jsx
+++ app/components/modules/themes/icons/icon-shapes.jsx
@@ -4,8 +4,8 @@ function IconShapes({slug}){
 	return(
 		<svg className={styles[slug]} width="113" height="87" viewBox="0 0 113 87" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<rect x="1.12134" y="0.592773" width="55.3808" height="55.3808" className="fill-bg stroke-accent-alt" strokeWidth="0.5"/>
-			<rect x="30.7822" y="31.5142" width="54.8808" height="54.8808" className="fill-bg" stroke="white"/>
-			<rect x="57.3318" y="1.34277" width="53.8808" height="53.8808" className="fill-bg stroke-accent" strokeWidth="2"/>
+			<rect x="30.7822" y="31.5142" width="54.8808" height="54.8808" className="fill-bg stroke-accent-alt" stroke="white"/>
+			<rect x="57.3318" y="1.34277" width="53.8808" height="53.8808" className="fill-accent-alt-faded stroke-accent" strokeWidth="3"/>
 		</svg>
 
 	)
