520c3f1621adeb196260a2195ba34e725ff8b737
Barry Richards <barry@br-web.me>
2024-10-28 21:31:24 +0000

    Set light/dark mode context and apply to compents

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "./assets/styles/main.scss";
 import Header from "./components/modules/header";
 import Footer from "./components/modules/footer";
+import { ColourThemeProvider } from "./context/colour-theme";
 
 export const metadata = {
   title: "Barry Richards",
@@ -11,9 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+		<ColourThemeProvider>
 		<Header/>
         {children}
 		<Footer/>
+		</ColourThemeProvider>
       </body>
     </html>
   );
