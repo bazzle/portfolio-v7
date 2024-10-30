836f575303155e4dca8626cac9a1f93f1e1dda65
Barry Richards <barry@br-web.me>
2024-10-30 23:29:12 +0000

    Mobile spacing tweaks

--- app/components/components/spacer/index.jsx
+++ app/components/components/spacer/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 function Spacer(props){
 	const classString = `${styles.spacer} ${props.mobile && styles.spacer_mobile}`;
 	return(
-		<div className={classString} role="decorative">
+		<div className={classString} role="presentation">
 		</div>
 	)
 }
