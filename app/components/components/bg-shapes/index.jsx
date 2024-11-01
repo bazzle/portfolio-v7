642e51ad11ec935d9c6dfd5a1826ac4ca7283928
Barry Richards <barry@br-web.me>
2024-11-01 18:06:26 +0000

    ..

--- app/components/components/bg-shapes/index.jsx
+++ app/components/components/bg-shapes/index.jsx
@@ -2,7 +2,6 @@ import styles from './style.module.scss';
 
 function BgShapes(props){
 	const variantClass = styles[`shapes_${props.variant}`];
-	console.log(styles);
 	return(
 		<div role="presentation" className={`${styles.shapes} ${variantClass}`}>
 			<div></div>
