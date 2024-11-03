4fbd163ac0382acc59dd3d1154e7ac885f5d4aff
Barry Richards <barry@br-web.me>
2024-11-03 22:44:08 +0000

    Animation first draft

--- app/components/components/bg-shapes/index.jsx
+++ app/components/components/bg-shapes/index.jsx
@@ -1,12 +1,36 @@
+'use client';
 import styles from './style.module.scss';
+import { motion } from 'framer-motion';
 
 function BgShapes(props){
 	const variantClass = styles[`shapes_${props.variant}`];
 	return(
 		<div role="presentation" className={`${styles.shapes} ${variantClass}`}>
 			<div></div>
-			<div></div>
-			<div></div>
+			<motion.div
+				initial={{ x: 0 }}
+				animate={{
+					x: [0, -100, 0]
+				}}
+				transition={{
+					duration: 30,
+					ease: "easeInOut",
+					repeat: Infinity,
+					repeatType: "loop"
+				}}
+			></motion.div>
+			<motion.div
+				initial={{ x: 0 }}
+				animate={{
+					y: [0, 50, 0],
+				}}
+				transition={{
+					duration: 60,
+					ease: "easeInOut",
+					repeat: Infinity,
+					repeatType: "loop"
+				}}
+			></motion.div>
 		</div>
 	)
 }
--- app/components/modules/hero/index.jsx
+++ app/components/modules/hero/index.jsx
@@ -1,8 +1,8 @@
 import styles from './style.module.scss';
-import VerticalDots from "./vertical-dots";
 import Contacts from "@/app/components/modules/contacts";
 import Clients from "@/app/components/modules/clients";
 import BgShapes from '@/app/components/components/bg-shapes';
+import SideFurniture from './side-furniture';
 
 function Hero(){
     return(
@@ -21,29 +21,8 @@ function Hero(){
 								... But still <span className="highlight">look cool</span>.
 							</span>
 						</p>
-						<div className={styles.side_furniture}>
-							<div className={styles.side_furniture__dots}>
-								<VerticalDots numDots = {4} />
-							</div>
-							<div className={styles.side_furniture__tick}>
-								<svg viewBox="0 0 70 55" xmlns="http://www.w3.org/2000/svg"><rect x="56.895" y="25.8069" width="18.2061" height="18.2061" transform="rotate(-135 56.895 25.8069)"/><rect x="13.1887" y="40.376" width="18.2061" height="18.2061" transform="rotate(-135 13.1887 40.376)"/><rect x="27.7576" y="54.9448" width="18.2061" height="18.2061" transform="rotate(-135 27.7576 54.9448)"/><rect x="27.7576" y="54.9448" width="18.2061" height="18.2061" transform="rotate(-135 27.7576 54.9448)"/><rect x="42.3264" y="40.3757" width="18.2061" height="18.2061" transform="rotate(-135 42.3264 40.3757)"/></svg>
-							</div>
-							<div className={styles.side_furniture__dots}>
-								<VerticalDots numDots = {2} />
-							</div>
-							<div className={styles.side_furniture__profile}>
-								<svg viewBox="0 0 25 23" xmlns="http://www.w3.org/2000/svg">
-									<mask id="profileMask">
-										<path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" fill="white" />
-									</mask>
-									<image
-									href="../assets/images/profile.jpg"
-									width="25"
-									height="25"
-									mask="url(#profileMask)"
-									/>
-								</svg>
-							</div>
+						<div className={styles.side_furniture_container}>
+							<SideFurniture/>
 						</div>
 					</div>
 				</div>
