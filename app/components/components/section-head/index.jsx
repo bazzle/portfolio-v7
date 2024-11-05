5067e730ecf42bba464d9c30a937eee7ecba4b3b
Barry Richards <barry@br-web.me>
2024-11-05 01:00:02 +0000

    Add stylised line component add to section head and animate

--- app/components/components/section-head/index.jsx
+++ app/components/components/section-head/index.jsx
@@ -1,19 +1,13 @@
 import propTypes from "prop-types";
 import styles from "./style.module.scss";
+import StylisedLine from "@/app/components/components/stylised-line";
 function SectionHead({
     titleString = 'Section title',
     separator = false
 }){
-	const separatorElem = () => {
-		return (
-			<div className={styles.separator}>
-				<svg viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" /></svg>
-			</div>
-		)
-	}
     return (
         <div className={styles.section_head}>
-			{separator && separatorElem()}
+			<StylisedLine/>
             <h2 className={separator ? styles.title : styles.title__no_separator}>
 				{titleString}
 			</h2>
---
+++ app/components/components/stylised-line/index.jsx
@@ -0,0 +1,43 @@
+"use client";
+import styles from './style.module.scss';
+import { motion } from "framer-motion";
+
+
+function StylisedLine(){
+	return (
+		<div className={styles.stylised_line} role="presentation">
+			<motion.div
+				className={styles.line}
+				initial={{ width: "30%", opacity: 0 }}
+				whileInView={{width: "100%", opacity: 1 }}
+				viewport={{ once: true }}
+				transition={{
+					duration: .01,
+					delay: .3,
+					type: "spring",
+					stiffness: 200,
+					damping: 30
+				}}
+			>
+			</motion.div>
+			<motion.svg
+				initial={{ opacity: 0, scale: 0.1 }}
+				whileInView={{ opacity: 1, scale: 1 }}
+				viewport={{ once: true }}
+				transition={{
+					duration: .02,
+					delay: .6,
+					type: "spring",
+					stiffness: 200,
+					damping: 30
+				}}
+				viewBox="0 0 25 25"
+				xmlns="http://www.w3.org/2000/svg"
+				>
+					<path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" />
+			</motion.svg>
+		</div>
+	)
+}
+
+export default StylisedLine;
