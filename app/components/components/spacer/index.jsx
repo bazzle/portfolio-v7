510ca1efb9d059197beb910e704b3a36d2ef0e3e
Barry Richards <barry@br-web.me>
2024-11-09 21:56:08 +0000

    Tweak mobile spacing

--- app/components/components/spacer/index.jsx
+++ app/components/components/spacer/index.jsx
@@ -1,7 +1,14 @@
 import styles from './style.module.scss';
 
-function Spacer({shallow, mobile, noLine}){
-	const classString =`${styles.spacer} ${mobile ? styles.spacer_mobile : ''} ${shallow ? styles.spacer_shallow : ''} ${noLine ? styles.spacer_noline : ''}`.trim();
+function Spacer({shallow, mobile, noMobile, noLine}){
+	let classString =(`
+		${styles.spacer}
+		${mobile ? styles.spacer_mobile : ''}
+		${noMobile ? styles.spacer_nomobile : ''}
+		${shallow ? styles.spacer_shallow : ''}
+		${noLine ? styles.spacer_noline : ''}
+		`).replace(/\s+/g, ' ');
+
 	return(
 		<div className={classString || undefined} role="presentation">
 		</div>
