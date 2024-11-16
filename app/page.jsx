8e77f0e4d55f987319a64af51eb0b4922fafd4ab
Barry Richards <barry@br-web.me>
2024-11-16 11:33:52 +0000

    Add light-mode styling for print

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-// import Image from "next/image";
 import Hero from "./components/modules/hero";
 import SkillsAndAbilities from "./components/modules/skills-and-abilities";
 import WorkHistory from "./components/modules/work-history";
@@ -9,6 +8,8 @@ import Typesheet from "./components/modules/type-sheet";
 import TextBlock from "./components/modules/text-block";
 import SignOff from "./components/modules/sign-off";
 import WhatNext from "./components/modules/what-next";
+
+
 
 function Home(){
 	return(
