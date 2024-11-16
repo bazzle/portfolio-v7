e5368a163d6987c1c37b0fc3c319e9f02f5ae0e0
Barry Richards <barry@br-web.me>
2024-11-16 23:49:28 +0000

    Add phone and desktop screenshots

--- app/components/modules/hero/contacts/index.jsx
+++ app/components/modules/hero/contacts/index.jsx
@@ -10,11 +10,17 @@ function Contacts() {
 	}
 	return (
 		<div className={styles.contacts}>
-			<div className={styles.inner}>
-				<ActionLink linkString="https://www.linkedin.com/in/br-web/" textString="LinkedIn" printString="linkedin.com/in/br-web/" />
-				<ActionLink linkString="https://github.com/bazzle/" textString="Github" printString="github.com/bazzle/" />
-				<ActionLink linkString="mailto:barry@br-web.me" textString="Drop me an email" printString="barry@br-web.me" />
-				<ActionLink classes="hide-print" clickEvent={PrintPage} textString="Print this page" />
+			<div className="container">
+				<div className={styles.inner}>
+					<div className={styles.contacts_section}>
+						<ActionLink linkString="https://www.linkedin.com/in/br-web/" textString="LinkedIn" printString="linkedin.com/in/br-web/" />
+						<ActionLink linkString="https://github.com/bazzle/" textString="Github" printString="github.com/bazzle/" />
+					</div>
+					<div className={styles.contacts_section}>
+						<ActionLink linkString="mailto:barry@br-web.me" textString="Drop me an email" printString="barry@br-web.me" />
+						<ActionLink classes="hide-print" clickEvent={PrintPage} textString="Print this page" />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
