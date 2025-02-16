0bf034ce3fa9ffa970e4d415939e68a11ce4f139
Barry Richards <barry@br-web.me>
2025-02-16 20:21:23 +0000

    Update icon designs and resize

---
+++ app/components/icons/icon-shapes.jsx
@@ -0,0 +1,14 @@
+'use client'
+import styles from "./style.module.scss";
+
+function IconShapes(){
+	return(
+		<svg className={styles.icon_shapes} width="125" height="98" viewBox="0 0 125 98" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect x="0.60625" y="0.862354" width="61.4137" height="61.4137" fill="#202020" stroke="#FFAAAA" stroke-width="1.2125"/>
+		<rect x="33.5674" y="35.2361" width="61.4137" height="61.4137" fill="#202020" stroke="white" stroke-width="1.2125"/>
+		<rect x="64.1555" y="2.2561" width="58.6262" height="58.6262" fill="#202020" stroke="#9AF3FF" stroke-width="4"/>
+		</svg>
+	)
+}
+
+export default IconShapes
