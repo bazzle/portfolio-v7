a5083681182e1f006b7d826653f7b7a0086da622
Barry Richards <barry@br-web.me>
2025-04-25 12:57:19 +0100

    Add title to colour mode switcher

--- app/components/modules/header/colour-mode.jsx
+++ app/components/modules/header/colour-mode.jsx
@@ -8,7 +8,7 @@ function ColourMode() {
 	const { colourMode, modeSwitch } = useContext(colourThemeContext);
 	const otherMode = colourMode === 'light' ? 'dark' : 'light';
 	return (
-		<button onClick={modeSwitch} aria-label={`Switch to ${otherMode} mode`} className={styles.dm_switcher}>
+		<button onClick={modeSwitch} title={`Switch to ${otherMode} mode`} aria-label={`Switch to ${otherMode} mode`} className={styles.dm_switcher}>
 
 			<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 				<path className="fill-foreground" fillRule="evenodd" clipRule="evenodd" d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24ZM12.0002 22C17.5229 22 22 18.0751 22 12C22 5.92487 17.5229 2 12.0002 2L12 12L12.0002 22Z"/>
