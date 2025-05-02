20f737a998370a15ac39b336b0e8cd604da81890
Barry Richards <barry@br-web.me>
2025-05-02 19:14:41 +0100

    Add back to top link back in

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         	{/* {children} */}
 			<Footer/>
 			</ColourThemeProvider>
-			{/* <BackTop/> */}
+			<BackTop/>
       	</body>
     </html>
   );
