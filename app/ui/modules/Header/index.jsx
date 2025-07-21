16a3ff9e314aa7a7a5cd3d339aa1501c4db25957
Barry Richards <barry@br-web.me>
2025-07-21 01:38:07 +0100

    Styling updates

--- app/ui/modules/Header/index.jsx
+++ app/ui/modules/Header/index.jsx
@@ -11,10 +11,6 @@ function Header(){
 						<span className='highlight'>{NameTitle.name}</span>
 						<span>{NameTitle.title}</span>
 					</h1>
-					<div className="header__line">
-						<div className="header__line__line"></div>
-						<svg viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><path d="M12.0208 24.0416L0 12.0208L12.0208 0L24.0416 12.0208L12.0208 24.0416Z" /></svg>
-					</div>
 				</div>
 			</div>
 			<ColourMode/>
