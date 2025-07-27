87037773adbcf9c4e2dd224c31518503d94d5073
Barry Richards <barry@br-web.me>
2025-07-27 23:33:34 +0100

    Make line extend line programatically, instead of CSS trickery

--- app/ui/modules/WorkHistory/index.jsx
+++ app/ui/modules/WorkHistory/index.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Section2 from '@/app/ui/components/Section2';
 import ContentSheet from "./ContentSheet";
 import { WorkHistoryContent } from '@/app/content/BodyContent';
@@ -11,9 +11,38 @@ function WorkHistory(){
 
 	const [activeSection, setActiveSection] = useState(sectionsArray[0]);
 
+	const tablistRef = useRef(null);
+	const currentItemRef = useRef(null);
+	const extendingLineRef = useRef(null);
+
+	function getTablistPercentage(){
+		const elem = tablistRef.current;
+		if(elem){
+			const width = Math.round(elem.getBoundingClientRect().width);
+			const start = Math.round(elem.getBoundingClientRect().x);
+			const activeItemPosition = Math.round(currentItemRef.current.getBoundingClientRect().x);
+			const percentage = Math.min(100, Math.max(0, ((activeItemPosition - start) / width) * 100));
+			return percentage;
+		}
+	}
+
+	useEffect(() => {
+		extendLine(getTablistPercentage());
+	});
+
+	function extendLine(inputWidth){
+		const elem = extendingLineRef.current;
+		if(elem){
+			elem.style.width = `${inputWidth}%`
+		}
+	}
+
 	function handleClick(evt, section){
 		setActiveSection(section);
 		evt.preventDefault();
+		setTimeout(()=>{
+			extendLine(getTablistPercentage());
+		},500);
 	}
 
 	function navItem(item, index){
@@ -32,14 +61,16 @@ function WorkHistory(){
 					<span className="workHistoryItem__title">{item.name}</span>
 					<span className="workHistoryItem__dates">{item.startDate} – {item.endDate}</span>
 				</span>
+				{isActive && <span className="indicator" ref={currentItemRef}></span> }
 			</button>
 		)
 	}
     return (
 		<Section2 moduleClassname="workHistory" heading={title} id="work-history" layout="2col">
 			<div className="workHistory__inner">
-				<nav role="tablist" className="workHistory__nav">
+				<nav ref={tablistRef} role="tablist" className="workHistory__nav">
 					{ sectionsArray.map((item, index) => navItem(item, index)) }
+					<div className="extending_line" ref={extendingLineRef}></div>
 				</nav>
 				{sectionsArray.map((item, index) => (
 					<ContentSheet
