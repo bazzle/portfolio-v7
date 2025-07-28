8fc3d324ad72bf8876697ac478995ed953d04da2
Barry Richards <barry@br-web.me>
2025-07-29 00:46:37 +0100

    Update skills

--- app/content/bodyContent.jsx
+++ app/content/bodyContent.jsx
@@ -71,12 +71,12 @@ export const SkillsContent = {
 		{ id: 4, name: 'Figma',  rating: 5 },
         { id: 5, name: 'Vanilla JS',  rating: 5 },
         { id: 6, name: 'Git',  rating: 5 },
-		{ id: 7, name: 'React/JSX',  rating: 3 },
-		{ id: 8, name: 'Next JS',  rating: 3 },
-		{ id: 9, name: 'TypeScript',  rating: 3 },
-		{ id: 10, name: 'Tailwind',  rating: 3 },
-        { id: 11, name: 'Vue',  rating: 2 },
-        { id: 12, name: 'nuxt',  rating: 2 },
+		{ id: 7, name: 'React/JSX',  rating: 4 },
+		{ id: 8, name: 'PHP',  rating: 4 },
+		{ id: 9, name: 'Next JS',  rating: 3 },
+		{ id: 10, name: 'TypeScript',  rating: 3 },
+		{ id: 11, name: 'Tailwind',  rating: 3 },
+        { id: 12, name: 'Vue',  rating: 2 },
 	]
 }
 
