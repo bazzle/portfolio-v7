84caaae4c4d8a12164112e1652115b9073474d8a
Barry Richards <barry@br-web.me>
2025-08-01 21:55:04 +0100

    Add ext link icon and animation

--- app/ui/modules/Cards/index.jsx
+++ app/ui/modules/Cards/index.jsx
@@ -45,7 +45,7 @@ function Cards(){
 				viewport={{ once: false }}
 				transition={{
 					duration: .01,
-					delay: .3,
+					delay: .5 + (index * 0.1),
 					type: "spring",
 					stiffness: 200,
 					damping: 30
@@ -53,6 +53,9 @@ function Cards(){
 				<div className="card__content">
 					<h2 id={`card-title-${index}`} className="card__header">
 						<a href={item.link} target='_blank'>{item.title}</a>
+						<svg className="extLink" width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M36 26V38C36 39.0609 35.5786 40.0783 34.8284 40.8284C34.0783 41.5786 33.0609 42 32 42H10C8.93913 42 7.92172 41.5786 7.17157 40.8284C6.42143 40.0783 6 39.0609 6 38V16C6 14.9391 6.42143 13.9217 7.17157 13.1716C7.92172 12.4214 8.93913 12 10 12H22M30 6H42M42 6V18M42 6L20 28" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" className='stroke-accent'/>
+						</svg>
 					</h2>
 					<p className='card__description'>{item.description}</p>
 				</div>
