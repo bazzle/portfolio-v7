262cfd53736dcb5052bbe0054008d1f1eff7d177
bazzle <barry@br-web.me>
2025-08-02 22:13:00 +0100

    Add empty 'Work With Me' page

---
+++ app/work-with-me/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Barry Richards Design tinkerer - Work with me",
+};
+
+function WorkWithMe() {
+  return (
+    <main></main>
+  );
+}
+
+export default WorkWithMe;
