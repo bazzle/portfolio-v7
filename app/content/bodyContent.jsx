28c423922e224b313292c39fed4b4c81e7acbe2b
Barry Richards <barry@br-web.me>
2025-08-22 22:35:24 +0100

    Placeholder

--- app/content/bodyContent.jsx
+++ app/content/bodyContent.jsx
@@ -345,11 +345,11 @@ export const ContentSection3 = {
 }
 
 export const GetInTouchContent = {
-	title: "Get in touch",
+	title: "Get in touch / Links",
 	bodyContent: () => {
 		const emailContent = 'email: <a href="mailto:barry@br-web.me">barry@br-web.me</a>';
 		const xContent = 'X: <a href="https://x.com/bazzle">@bazzle</a>';
-		const nostrContent = 'nostr: <a href="https://nosta.me/27a3d96de5945c3e9e522e63738ed289b1cfde8a7eca1fa31a953c3b23c87908">nosta.me</a>';
+		const nostrContent = 'Coming soon';
 		const linkedInContent = '<a href="https://www.linkedin.com/in/br-web">LinkedIn</a>';
 		const cvContent = '<a href="./files/cv.pdf">CV</a>'
 		const gitHubContent = '<a href="https://github.com/bazzle">GitHub</a>'
