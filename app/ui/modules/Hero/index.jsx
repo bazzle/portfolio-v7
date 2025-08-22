cbde16b31389dd2c10a55159795dfd3dd3b90198
Barry Richards <barry@br-web.me>
2025-08-22 20:07:47 +0100

    ..

--- app/ui/modules/Hero/index.jsx
+++ app/ui/modules/Hero/index.jsx
@@ -37,7 +37,7 @@ function Hero(){
 							<div className="hero__footSection__intro__main">
 								<p>
 									<strong>Hello!</strong> I’m a UK based web designer, developer and accessibility consultant.<br/>
-									<Link href="#who-i-am-and-what-i-do">Read on</Link> to learn more about me, or <Link href="#get-in-touch">Get in touch</Link>!
+									<Link href="#who-i-am-and-what-i-do">Read on</Link> to learn more about me, <Link href="./files/cv.pdf">download my CV</Link> or <Link href="#get-in-touch">Get in touch</Link>!
 								</p>
 							</div>
 						</div>
