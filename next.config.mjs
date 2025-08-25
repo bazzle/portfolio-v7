ef7ff7289c9dfea3f59026620c70753781a9ed09
Barry Richards <barry@br-web.me>
2025-08-25 22:52:57 +0100

    Update nextjs and packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  basePath: isProd ? '' : '',
-  output: 'export',
-  env: {
-    ASSET_PREFIX: isProd ? '' : '',
-  }
+	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
+	basePath: isProd ? '' : '',
+	output: 'export',
+	env: {
+		ASSET_PREFIX: isProd ? '' : '',
+	},
 };
 
 export default nextConfig;
