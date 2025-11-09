c530a4c759823a26c6c1dc2684e147c3cbcc4616
Barry Richards <barry@br-web.me>
2025-11-09 18:23:38 +0000

    Add checks for metadata

--- app/ui/components/blog/MetaData/index.jsx
+++ app/ui/components/blog/MetaData/index.jsx
@@ -1,13 +1,24 @@
+'use client'
+
 import PostDate from "@/app/ui/components/blog/PostDate";
 import TagList from "@/app/ui/components/blog/TagList";
 import styles from './MetaData.module.scss';
+import { useContext } from "react";
+import { HomepageContext } from "@/app/context/HomepageCheck";
 
 function MetaData({tags, date}){
+	const {isHome} = useContext(HomepageContext);
+	
+	const tagsList = (
+		<>
+			<span className={styles.divider}></span>
+			<TagList tags={tags} />
+		</>
+	)
 	return(
 		<div className={styles.metadata}>
 			<PostDate rawDate={date} />
-			{tags && <span className={styles.divider}></span>}
-			{tags && <TagList tags={tags} />}
+			{(tags && isHome) && tagsList}
 		</div>
 	)
 }
