f193a166b94647ff34dc6b61059bd2660c2d9fbf
Barry Richards <barry@br-web.me>
2026-03-22 13:04:23 +0000

    Force dark mode for now

--- app/context/ThemeProvider.jsx
+++ app/context/ThemeProvider.jsx
@@ -4,7 +4,8 @@ import { ThemeProvider } from 'next-themes'
 
 export default function AppThemeProvider({ children }) {
 	return (
-		<ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
+		// Set defaultTheme to "system" when I'm ready for light-mode
+		<ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem>
 			{children}
 		</ThemeProvider>
 	)
