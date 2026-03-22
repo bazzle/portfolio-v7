5cad30561c0b60b7bc0c7d1f324e27fff72e0833
Barry Richards <barry@br-web.me>
2026-03-22 13:08:18 +0000

    Set the theme to system

--- app/context/ThemeProvider.jsx
+++ app/context/ThemeProvider.jsx
@@ -4,8 +4,7 @@ import { ThemeProvider } from 'next-themes'
 
 export default function AppThemeProvider({ children }) {
 	return (
-		// Set defaultTheme to "system" when I'm ready for light-mode
-		<ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem>
+		<ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
 			{children}
 		</ThemeProvider>
 	)
