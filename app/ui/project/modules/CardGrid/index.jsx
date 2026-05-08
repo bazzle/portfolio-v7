8ccba1210c3f98e529adc2552446931998a266e0
Barry Richards <barry@br-web.me>
2026-05-08 16:20:53 +0100

    Install masonry package, and styling updates to card grid

--- app/ui/project/modules/CardGrid/index.jsx
+++ app/ui/project/modules/CardGrid/index.jsx
@@ -1,7 +1,14 @@
 'use client'
+import dynamic from 'next/dynamic'
 import styles from './CardGrid.module.scss'
 import Section from '@/app/ui/project/components/Section'
 import Card from '@/style-library/components/Card'
+
+const ResponsiveMasonry = dynamic(
+	() => import('react-responsive-masonry').then((m) => m.ResponsiveMasonry),
+	{ ssr: false },
+)
+const Masonry = dynamic(() => import('react-responsive-masonry'), { ssr: false })
 
 export default function CardGrid({
 	line,
@@ -24,20 +31,25 @@ export default function CardGrid({
 	return (
 		<Section heading={title} headingTop {...optionalProps} layout="extended">
 			<div className={styles.cardGrid}>
-				{cards.map((item, index) => (
-					<div className={styles.cardGrid__item} key={index}>
-						<Card
-							imageSrc={item.imageSrc}
-							imageAlt={item.imageAlt}
-							link={item.link}
-							title={item.title}
-							description={item.description}
-							index={index}
-							basic={index > 2}
-							extLink={item.extLink}
-						/>
-					</div>
-				))}
+				<ResponsiveMasonry columnsCountBreakPoints={{ 359: 1, 641: 2, 880: 3 }} gutterBreakPoints={{ 359: 'clamp(0.5rem, 2.5vw, 5rem)' }}>
+					<Masonry gutter="20px">
+						{cards.map((item, index) => (
+							<div className={styles.cardGrid__item} key={index}>
+								<Card
+									key={index}
+									imageSrc={item.imageSrc}
+									imageAlt={item.imageAlt}
+									link={item.link}
+									title={item.title}
+									description={item.description}
+									index={index}
+									basic={index > 2}
+									extLink={item.extLink}
+								/>
+							</div>
+						))}
+					</Masonry>
+				</ResponsiveMasonry>
 			</div>
 			{children}
 		</Section>
